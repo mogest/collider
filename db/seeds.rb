dfc6d78eac757acc7143c65992e320d23a497d2a
Roger Nesbitt <roger@seriousorange.com>
2015-06-18 14:53:14 +1200

    Add a demo atom to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,32 @@ end
 
 a = Account.create!(name: "Powershop Collider")
 
+user = a.users.create!(email: "roger.nesbitt@powershop.co.nz")
+
 elements = %w(Epic Story Deliverable Task Issue).map do |name|
   a.elements.create!(name: name)
 end
 
 epic, story, dlvr, task, issue = elements 
 
+title = a.fields.create!(name: "Title", field_type: "long", data_type: "text")
 description = a.fields.create!(name: "Description", field_type: "textarea", data_type: "text")
 priority = a.fields.create!(name: "Priority", field_type: "small", data_type: "integer")
 
 elements.each do |element|
-  element.element_fields.create!(field: description)
-  element.element_fields.create!(field: priority)
+  [title, description, priority].each do |field|
+    element.element_fields.create!(field: field)
+  end
 end
+
+data = {
+  "element_id" => story.id,
+  "properties" => {
+    title.id => "Write more tests",
+    priority.id => 2,
+    description.id => "It's important to write tests otherwise we won't know when we've borken things."
+  }
+}
+
+atom = PopulateAtom.new(nil, data, user: user)
+raise atom.errors.full_messages.to_sentence unless atom.call
